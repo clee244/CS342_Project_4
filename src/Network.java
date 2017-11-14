694a05a5c61aaeeb8200d12c35af4b55ce31f063
clee244 <clee244@uic.edu>
2017-11-14 14:57:20 -0600

    Network skeleton?

--- src/Network.java
+++ src/Network.java
@@ -1,3 +1,52 @@
-public class Network {
+import java.net.*;
+import java.io.*;
 
+public class Network {
+    public Network()throws IOException{
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(10007);
+        }
+        catch (IOException e)
+        {
+            System.err.println("Could not listen on port: 10007.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+
+        try {
+            System.out.println ("Waiting for Client");
+            clientSocket = serverSocket.accept();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        ObjectOutputStream out = new ObjectOutputStream(
+                clientSocket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(
+                clientSocket.getInputStream());
+
+        Point2d pt2 = null;
+
+        try {
+            pt2 = (Point2d) in.readObject();
+        }
+        catch (Exception ex)
+        {
+            System.out.println (ex.getMessage());
+        }
+        out.writeObject(pt2);
+        out.flush();
+
+
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 }
